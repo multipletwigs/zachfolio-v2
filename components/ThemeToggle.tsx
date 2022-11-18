c27d72c245031bf90036945606bfecd82f95813b
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2022-11-19 00:21:59 +0800

    fixed minor toggle color

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -16,7 +16,7 @@ export const ThemeToggle = () => {
     <>
       {mounted && (
         <button
-          className="inline-flex items-center gap-2 rounded bg-slate-200 py-2 px-4 font-bold text-slate-700 transition-colors duration-300 hover:outline hover:outline-2 hover:outline-slate-700 dark:bg-slate-700 dark:text-slate-200"
+          className="inline-flex items-center gap-2 rounded bg-slate-200 py-2 px-4 font-bold text-slate-700 transition-colors duration-300 hover:outline hover:outline-2 dark:hover:outline-white dark:bg-slate-700 dark:text-slate-200"
           onClick={() => {
             setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
           }}
