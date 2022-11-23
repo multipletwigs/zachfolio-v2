578c617d3e53ae3363f5db7cb18697b8836498a5
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2022-11-23 13:52:58 +0800

    Added in twitter:card property

--- layouts/Container.tsx
+++ layouts/Container.tsx
@@ -40,7 +40,8 @@ export function Container(props: any) {
           content={`${siteMetaData.siteUrl}${router.asPath}`}
         />
         {/* Twitter is a bit special, for twitter:image to work, give the meta tag a NAME property instead of PROPERTY property */}
-        <meta name="twitter:card" content="https://zachkhong.vercel.app/api/ogImage"></meta>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:image" content="https://zachkhong.vercel.app/api/ogImage"></meta>
       </Head>
       <NavBar />
       <main
